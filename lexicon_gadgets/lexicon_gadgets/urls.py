933af31ffa522c50528c89ee94c92155cf8ca114
Taza53 <smurtaza.53@outlook.com>
2022-11-14 12:58:47 +0100

    login base

--- lexicon_gadgets/lexicon_gadgets/urls.py
+++ lexicon_gadgets/lexicon_gadgets/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from lexiconapp import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(views.user_login, name='user_login'),
 ]
