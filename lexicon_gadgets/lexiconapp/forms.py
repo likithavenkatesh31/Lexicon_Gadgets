217ec6512bed2b7f5e495beb91b4fb4b7d98fd8c
Taza53 <smurtaza.53@outlook.com>
2022-11-14 13:00:45 +0100

    user login form

--- lexicon_gadgets/lexiconapp/forms.py
+++ lexicon_gadgets/lexiconapp/forms.py
@@ -1,3 +1,10 @@
 from django import forms
 from django.core import validators
 from django.contrib.auth.models import User
+
+class UserLogin(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username','password')
