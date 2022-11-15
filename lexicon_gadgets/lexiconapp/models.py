0d11d604abd1a0416f8c6bdbaa305403de51de13
likithavenkatesh31 <likitha31venkatesh@gmail.com>
2022-11-15 15:11:52 +0100

    changes to models.py

--- lexicon_gadgets/lexiconapp/models.py
+++ lexicon_gadgets/lexiconapp/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 from django.contrib.auth.models import User
 # Create your models here.
+class Contact(models.Model):
+     sno= models.AutoField(primary_key=True)
+     name= models.CharField(max_length=255)
+     phone= models.CharField(max_length=13)
+     email= models.CharField(max_length=100)
+     content= models.TextField()
+     timeStamp=models.DateTimeField(auto_now_add=True, blank=True)
