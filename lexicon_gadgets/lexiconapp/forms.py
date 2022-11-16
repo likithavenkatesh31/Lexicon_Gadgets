ee6abcbee23be86b2d242ba8cb17d34a93f69ab5
arunbhalli <tomarbhalli@gmail.com>
2022-11-16 19:27:56 +0100

    adds new views for signup

--- lexicon_gadgets/lexiconapp/forms.py
+++ lexicon_gadgets/lexiconapp/forms.py
@@ -1,17 +1,13 @@
 from django import forms
-from django.core import validators
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+
 
 class UserLogin(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
-
     class Meta():
         model = User
-        fields = ('username','password')
-        
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+        fields = ('username', 'password')
 
-    class Meta():
-        model = User
-        fields = ('username', 'email', 'password')
+
+
--- lexicon_gadgets/lexiconapp/urls.py
+++ lexicon_gadgets/lexiconapp/urls.py
@@ -10,10 +10,7 @@ urlpatterns = [
     path('logout/', views.userlogout, name='userlogout'),
     path('card',views.card, name='card'),
     path("signup", views.signup, name='signup'),
-
     path('orders/', views.orderbyuser, name='orders'),
-
-
     path('contact/',views.contact, name='contact'),
 
 ]
